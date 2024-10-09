405a62e1adcff3b96cbe7eb125accfca936fac17
alauddinkochi <alauddinkochi09@gmail.com>
2024-10-10 02:16:56 +0600

    added partially event handler

--- js/home.js
+++ js/home.js
@@ -1,1 +1,14 @@
-console.log('home')
+// add money to the account
+
+// step-1-add an event handler to the add money button inside the form
+document.getElementById('btn-add-money').addEventListener('click', function(event){
+    event.preventDefault();
+    
+
+    // step-2- get money added to the account
+    const addMoneyInput = document.getElementById('input-add-money').value;
+    console.log(addMoneyInput);
+
+    // get the pi number provided
+    const pinNumberInput = document.getElementById('input-pin-number').value;
+})
