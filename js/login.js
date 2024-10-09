66e16cf204e5752aa4df56da065d52857b266bce
alauddinkochi <alauddinkochi09@gmail.com>
2024-10-10 01:57:40 +0600

    added structure

--- js/login.js
+++ js/login.js
@@ -4,10 +4,19 @@
 document.getElementById('btn-login').addEventListener('click', function(event){
     // 2. prevent default behavior (reloading)
     event.preventDefault();
-    console.log('btn clicked');
+    
 
     // 3.get the phone number
 
     const phoneNumber = document.getElementById('phone-number').value;
-    console.log(phoneNumber)
+    const pinNumber = document.getElementById('pin-number').value;
+    console.log(phoneNumber, pinNumber)
+
+    if(phoneNumber === '5' && pinNumber === '1234'){
+        console.log('you are logged in');
+        window.location.href = '/home.html'
+    }
+    else{
+        alert('wrong phone number & pin number');
+    }
 })
