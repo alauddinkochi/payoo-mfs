cb6ee26063ea02a471707d3880171a44b56bd6ca
alauddinkochi <alauddinkochi09@gmail.com>
2024-10-10 02:39:39 +0600

    added balance form successfully'

--- js/home.js
+++ js/home.js
@@ -9,6 +9,31 @@ document.getElementById('btn-add-money').addEventListener('click', function(even
     const addMoneyInput = document.getElementById('input-add-money').value;
     console.log(addMoneyInput);
 
-    // get the pi number provided
+    // get the pin number provided
     const pinNumberInput = document.getElementById('input-pin-number').value;
+    console.log(pinNumberInput);
+
+    // step-3- verify the pin number
+    if(pinNumberInput === '1234'){
+        console.log('added money to your account');
+
+        // step-4 get the current amount
+    const balance = document.getElementById('account-balance').innerText;
+    console.log(balance);
+
+    // step-5 add addMoneyInput with balance
+    const addMoneyNumber = parseFloat(addMoneyInput);
+    const balanceNumber = parseFloat(balance);
+    const newBalance = addMoneyNumber + balanceNumber;
+    console.log(newBalance);
+
+    // step-6 update the balance at ui
+
+    document.getElementById('account-balance').innerText = newBalance;
+    }
+    else{
+        alert('failed to added money')
+    }
+
+    
 })
